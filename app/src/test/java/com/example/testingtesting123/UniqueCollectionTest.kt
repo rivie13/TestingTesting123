1bdc988bd77a93cf994cc0d609a7ca6eb3dc5e81
Riviera Sperduto <riviera.t.sperduto13@gmail.com>
2025-04-15 12:03:32 -0400

    TDD implementation
    - Added tests for adding single and unique items to the collection.
    - Implemented addItem to add items to the collection.
    - Implemented unique item handling to prevent duplicate additions.
    - Updated gradle version from 8.9 to 8.10.2
    - Update bytecode target level from 17 to 21
    - Removed redundant xml declaration
    - Implemented clear function for clearing collection

--- app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
+++ app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
@@ -1,5 +1,6 @@
 package com.example.testingtesting123
 
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 
@@ -16,14 +17,31 @@ class UniqueCollectionTest {
     // TODO 1: Write a test to ensure items can be added to the collection
     @Test
     fun addAnItem() {
+        // Given
+        val item1 = Item("Test Item")
 
+        // When
+        collection.addItem(item1)
+
+        // Then
+        Assert.assertEquals(1, collection.size()) // Verify that the collection size increased
+        Assert.assertEquals(item1, collection.get(0)) // Verify that the item was added and is at index 0
     }
 
     // TODO 2: Write a test to ensure that only unique items can be added to the collection
     // Uniqueness is determined by the Item.name property, which is set via the constructor
     @Test
     fun addUniqueItem() {
+        //Given
+        val item1 = Item("Test Item")
+        val item2 = Item("test item")
 
+        //When
+        collection.addItem(item1)
+        collection.addItem(item2)
+
+        //Then
+        Assert.assertEquals(1, collection.size())
     }
 
     // Test Driven Development (TDD) test - complete specified function so that this test passes
